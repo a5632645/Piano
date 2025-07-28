6e6d8c11bc151c7de08e06e38d08b67bc031f09e
a5632645 <3022602213@qq.com>
2025-07-28 14:38:17 +0800

    keep code style

--- plugin/Source/dwgs.cpp
+++ plugin/Source/dwgs.cpp
@@ -303,9 +303,7 @@ void dwgs::set(float Fs, int longmodes, int downsample, int upsample, float f, f
         if(delTab) {
             float n = float (PI) * k / delHalf;
             // if(modeTable[k]) delete modeTable[k];
-            if (modeTable[k]) {
-                _aligned_free(modeTable[k]);
-            }
+            if (modeTable[k]) _aligned_free(modeTable[k]);
             posix_memalign ((void**)&modeTable[k], 32, size_t (delTab + 8) * sizeof (float));
 
             for (int i = 0; i <= delTab; i++)
